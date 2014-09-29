ceba1a735996ae2ee78264f8c4cd5d95acc3eba6
waylau <waylau521@gmail.com>
2014-09-29 23:51:57 +0800

    tabs  option

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,10 +12,11 @@ var initLeftMenu = function(){
         success: function( result ) {
             if( result.data ){
                 var data = result.data;
+                var accd ='';
                 $.each( data,function( i, item ){
                     var group = item.group ;
                     var gorupData = item.data;
-                    var accd = '<div  title="'+group+'" style="border:0"><ul>';
+                    accd += '<div  title="'+group+'" ><ul>';
 
                     $.each( gorupData,function( i, item ){
                         var title = item.title;
@@ -28,10 +29,10 @@ var initLeftMenu = function(){
 
                     });
 
-                    accd += '</di></ul>';
-
-                    $("#left-menu-content-id").append( accd );
+                    accd += '</ul></div>';
                 })
+                $("#left-menu-content-id").html( accd );
+                $.parser.parse();
             }
 
         },
@@ -70,6 +71,8 @@ var addTabContent = function (title, url, closableValue) {
         });
     }
 
+    tabClose();
+
 };
 
 /**
@@ -89,8 +92,96 @@ var addTabHref = function (title, url, closableValue) {
             closable: closableValue
         });
     }
+
+    tabClose();
 };
 
+
+function tabClose()
+{
+    /*双击关闭TAB选项卡*/
+    $(".tabs-inner").dblclick(function(){
+        var subtitle = $(this).children(".tabs-closable").text();
+        $('#tabs').tabs('close',subtitle);
+    })
+    /*为选项卡绑定右键*/
+    $(".tabs-inner").bind('contextmenu',function(e){
+        $('#tab_memu_id').menu('show', {
+            left: e.pageX,
+            top: e.pageY
+        });
+
+        var subtitle =$(this).children(".tabs-closable").text();
+
+        $('#tab_memu_id').data("currtab",subtitle);
+        $('#tabs').tabs('select',subtitle);
+        return false;
+    });
+}
+//绑定右键菜单事件
+function tabCloseEven()
+{
+    //刷新
+    $('#mm-tabupdate').click(function(){
+        var currTab = $('#tabs').tabs('getSelected');
+        var url = $(currTab.panel('options').content).attr('src');
+        $('#tabs').tabs('update',{
+            tab:currTab,
+            options:{
+                url:url
+            }
+        })
+    })
+    //关闭当前
+    $('#mm-tabclose').click(function(){
+        var currtab_title = $('#tab_memu_id').data("currtab");
+        $('#tabs').tabs('close',currtab_title);
+    })
+    //全部关闭
+    $('#mm-tabcloseall').click(function(){
+        $('.tabs-inner span').each(function(i,n){
+            var t = $(n).text();
+            $('#tabs').tabs('close',t);
+        });
+    });
+    //关闭除当前之外的TAB
+    $('#mm-tabcloseother').click(function(){
+        $('#mm-tabcloseright').click();
+        $('#mm-tabcloseleft').click();
+    });
+    //关闭当前右侧的TAB
+    $('#mm-tabcloseright').click(function(){
+        var nextall = $('.tabs-selected').nextAll();
+        if(nextall.length==0){
+            //msgShow('系统提示','后边没有啦~~','error');
+            alert('后边没有啦~~');
+            return false;
+        }
+        nextall.each(function(i,n){
+            var t=$('a:eq(0) span',$(n)).text();
+            $('#tabs').tabs('close',t);
+        });
+        return false;
+    });
+    //关闭当前左侧的TAB
+    $('#mm-tabcloseleft').click(function(){
+        var prevall = $('.tabs-selected').prevAll();
+        if(prevall.length==0){
+            alert('到头了，前边没有啦~~');
+            return false;
+        }
+        prevall.each(function(i,n){
+            var t=$('a:eq(0) span',$(n)).text();
+            $('#tabs').tabs('close',t);
+        });
+        return false;
+    });
+
+    //退出
+    $("#mm-exit").click(function(){
+        $('#tab_memu_id').menu('hide');
+    })
+}
 /**
  * 弹窗
  */
@@ -136,6 +227,12 @@ $(document).ready(function () {
             }
         });
     });
-
+    /**
+     * 监听tab 右键事件
+     */
+    $(function(){
+        tabClose();
+        tabCloseEven();
+    })
 
 });
