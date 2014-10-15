cb33d5239c1f4f20b38ffdb7d498e7aef73601ee
waynelau <waynelau521@gmail.com>
2014-10-15 15:59:27 +0800

    增加  msg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -260,65 +260,6 @@ function tabCloseEven()
         $('#tab_memu_id').menu('hide');
     })
 }
-/**
- * 弹窗
- */
-var msgShow = function (title, msg,showType,timeout,showSpeed,width,height) {
-    $.messager.show({
-        title:title,
-        msg:msg,
-        timeout:timeout,
-        showSpeed:showSpeed,
-        showType:showType,
-        width:width,
-        heigh:height
-    });
-};
-
-/**
- * 警告窗 e=error, q=question,i=info,w=warning.
- */
-var msgAlert = function (title, msg, icon, fn) {
-
-    if ( /^e/.test(icon)) {
-        icon = 'error';
-    }else if ( /^q/.test(icon)) {
-        icon = 'question';
-    }else if ( /^i/.test(icon)) {
-        icon = 'info';
-    }else if ( /^w/.test(icon)) {
-        icon = 'warning';
-    }
-
-    $.messager.alert(
-        title,
-        msg,
-        icon,
-        fn
-    );
-};
-
-/**
- * 确认窗
- */
-var msgConfirm = function (title, msg, fn) {
-    $.messager.confirm(
-        title,
-        msg,
-        fn
-    );
-};
-
-/**
- * 提示窗
- */
-var msgPrompt = function (title, msg, fn) {
-    $.messager.prompt(
-        title,
-        msg,
-        fn
-    );
-};
 
 
 $(document).ready(function () {
