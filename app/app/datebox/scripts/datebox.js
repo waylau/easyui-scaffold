90ad5a7caf0773e74391723951e1a2919928f938
waylau <waylau521@gmail.com>
2015-04-16 18:12:55 +0800

    add  datebox

---
+++ app/app/datebox/scripts/datebox.js
@@ -0,0 +1,23 @@
+/**
+ * Created by waylau.com on 2015-4-16
+ */
+function myformatter(date){
+    var y = date.getFullYear();
+    var m = date.getMonth()+1;
+    var d = date.getDate();
+    return y+'-'+(m<10?('0'+m):m);
+}
+
+
+function myparser(s){
+    if (!s) return new Date();
+    var ss = (s.split('-'));
+    var y = parseInt(ss[0],10);
+    var m = parseInt(ss[1],10);
+    var d = parseInt(ss[2],10);
+    if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
+        return new Date(y,m-1,d);
+    } else {
+        return new Date();
+    }
+}
